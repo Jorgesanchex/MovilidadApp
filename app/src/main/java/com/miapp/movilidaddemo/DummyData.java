8bdfd22524b1444d7cb3d7a270ce1432543c2d1d
Jorgesanchex <jtrujillo59@uabc.edu.mx>
2026-01-18 14:58:44 -0800

    Create DummyData.java

---
+++ app/src/main/java/com/miapp/movilidaddemo/DummyData.java
@@ -0,0 +1,10 @@
+package com.miapp.movilidaddemo;
+
+public class DummyData {
+
+    public static String nombre = "María López";
+    public static String fechaNacimiento = "15/03/1995";
+    public static String telefono = "664-555-1234";
+    public static String email = "maria.lopez@example.com";
+    public static String descripcion = "Contacto de prueba para demo MovilidadApp";
+}
